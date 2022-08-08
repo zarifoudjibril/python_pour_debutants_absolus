9cb76f6c4f35bd9feb7f1ad3393f445e3675c755
zarifoudjibril <67414124+zarifoudjibril@users.noreply.github.com>
2022-08-08 16:41:22 +0000

    Create exercice_1.py

---
+++ chapitre_3/exercices/exercice_1.py
@@ -0,0 +1,11 @@
+# Cet exercice est adopté du livre "Python for Everybody" écris par Dr. Charles R. Severance et traduit en Français et modifié par Zarifou Djibril (https://medium.com/@zarifoud). Lien: http://do1.dr-chuck.com/pythonlearn/EN_us/pythonlearn.pdf
+
+# Exercice 1 : écrivez un programme qui demande à l'utilisateur d'entrer son nom, ensuite le dit bonjour.
+
+# Entrez votre nom : Chuck
+# Bonjour Chuck
+
+
+# Réponse :
+nom = input('Entrez votre nom : ')
+print('Bonjour ' + nom)
